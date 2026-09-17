9d37bc4114d131f6eed0fe48945f7e4e549a68e9
KavipriyaDK <kavipriya.d2024@vitstudent.ac.in>
2026-09-17 09:27:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 with open("report.txt", "w") as f:
     f.write("Application Report\n")
-    f.write("Total Users: 120\n")
+    f.write("Total Users: 300\n")
     f.write("Active Users: 45\n")
 
 print("Report generated.")
